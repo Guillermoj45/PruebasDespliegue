935d41d6e0c4c03e86642d5b4d2e8535816dd103
Guillermo <guillermoremesa@gmail.com>
2024-12-21 21:02:04 +0100

    inicio

--- PruebasDespliegue/tests.py
+++ PruebasDespliegue/tests.py
@@ -5,7 +5,3 @@ class HomePageTests(TestCase):
     def test_home_page_status_code(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-
-    def test_home_page_template(self):
-        response = self.client.get(reverse('home'))
-        self.assertTemplateUsed(response, 'home.html')
