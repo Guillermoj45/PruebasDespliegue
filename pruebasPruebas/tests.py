8f16328ce6b2dad820176c0b28a0bad9b7407532
Guillermo <guillermoremesa@gmail.com>
2024-12-22 19:20:12 +0100

    conexion con la base de datos

--- pruebasPruebas/tests.py
+++ pruebasPruebas/tests.py
@@ -1,3 +1,18 @@
+from time import sleep
+
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class CreateUsuario(TestCase):
+    def test_create_usuario(self):
+        usuario = Usuario(username='test', password='test', email='pedro@safareyes.es')
+        usuario.save()
+        self.assertEqual(usuario.username, 'test')
+        self.assertEqual(Usuario.objects.get(username='test').username, 'test')
+
+    def test_create_usuario2(self):
+        usuario = Usuario(username='test', password='test', email='pedsad@safareyes.es')
+        usuario.save()
+        self.assertEqual(usuario.username, 'test')
